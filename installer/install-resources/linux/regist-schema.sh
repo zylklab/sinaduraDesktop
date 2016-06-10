c4b866587d5bbd288cd666b8d4c586f016a58e2f
gfg001 <gfg001@3279b7b3-46a5-0410-b095-b22bd0dda68d>
2016-06-10 07:03:37 +0000

    cambio para que los .sh sean ejecutables ... que no se por qué en el nuevo izpack no consigo hacerlo con <executable>
    git-svn-id: https://services.zylk.net/sinadura-repo/sinadura/sinaduraDesktop/trunk@1027 3279b7b3-46a5-0410-b095-b22bd0dda68d

--- installer/install-resources/linux/regist-schema.sh
+++ installer/install-resources/linux/regist-schema.sh
@@ -5,3 +5,8 @@ sed -i.bak-blank '/^$/d' $HOME/.local/share/applications/mimeapps.list
 
 #añado a la lista de mimetypes la nueva entrada 
 echo "x-scheme-handler/sinadura=sinadura-desktop-protocol.desktop" >> $HOME/.local/share/applications/mimeapps.list
+
+
+#como ha dejado de funcionar que izpack marque los ficheros como +x lo hago a mano
+chmod +x $INSTALL_PATH/bin/sinadura.sh
+chmod +x $INSTALL_PATH/bin/sinadura-console.sh
